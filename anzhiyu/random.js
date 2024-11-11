85f8eaa87c289c050b750da6d1f8601d371772e4
sanjiu2024 <3375927974@qq.com>
2024-11-11 21:07:22 +0800

    Site updated: 2024-11-11 21:07:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/09/openfrp穿透教程/","2024/11/09/樱花穿透教程/","2024/11/09/zzcraft又复活了/"];function toRandomPost(){
+var posts=["2024/11/09/openfrp穿透教程/","2024/11/09/樱花穿透教程/","2024/11/11/我的世界java保姆级教程/","2024/11/09/zzcraft又复活了/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
